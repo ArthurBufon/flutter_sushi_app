84a7a3dd041bcf0196eea34a3c6cf1367fb6fbda
Arthur <arthurbufon2052@gmail.com>
2023-09-14 20:29:45 -0300

    Fixed main method error.

--- lib/main.dart
+++ lib/main.dart
@@ -2,5 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:flutter_sushi_app/pages/home_page.dart';
 
 void main() {
-  runApp(const HomePage());
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp ({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Sushi App',
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
+      ),
+      home: const HomePage(),
+    );
+  }
 }
